5973321c3175abdf2b7625c00626cfd3318f1efd
ameyakore <kore.ameya@gmail.com>
2022-09-05 22:39:53 +0530

    testing phase

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_attribute]
+pub fn impl_gen(attr: TokenStream, item: TokenStream) -> TokenStream {
+    
+    println!("{:#}", attr);
+
+    let attr_ast = syn::parse(attr).unwrap();
+    let struct_ast = syn::parse(item).unwrap();
+
+    impl_gen_macro(&attr_ast, &struct_ast);
+
+    todo!()
+
+}
+
+fn impl_gen_macro(attr_ast: &syn::DeriveInput, struct_ast: &syn::DeriveInput) -> TokenStream {
+    let name = &struct_ast.ident;
+    // println!("{:?}", attr_ast);
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
