54e30846b08113a0d69256dda12d1773d5dbc2dc
ameyakore <kore.ameya@gmail.com>
2022-09-05 23:22:49 +0530

    works now

--- src/lib.rs
+++ src/lib.rs
@@ -21,25 +21,28 @@ pub fn impl_gen(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     // let attr_ast = syn::parse(attr).expect("Attr failed");
     // println!("Hello");
-    let struct_ast = syn::parse(item).expect("Struct failed");
-    println!("Hello pt2");
+    // let struct_ast = syn::parse(item).expect("Struct failed");
+    // println!("Hello pt2");
 
-    impl_gen_macro(&attr, &struct_ast);
+    impl_gen_macro(&attr, &item);
 
     todo!()
 
 }
 
-fn impl_gen_macro(attrs: &TokenStream, struct_ast: &syn::DeriveInput) -> TokenStream {
+fn impl_gen_macro(attrs: &TokenStream, structs: &TokenStream) -> TokenStream {
     let str_name = attrs.to_string();
     let names = str_name.split(',').collect::<Vec<_>>();
-    let name = &struct_ast.ident;
+    // let name = &struct_ast.ident;
+    for t in structs.into_iter() {
+        println!("{}", t);
+    };
     let fn_name = format_ident!("get_{}", names[1].trim().to_case(Case::Snake));
     let trait_name = format_ident!("{}", names[0].trim());
     let gen = quote! {
-        impl #trait_name for #name {
-            fn #fn_name() {
-                println!("Hello, Macro! My name is {}!", stringify!(#name));
+        impl #trait_name for Asd {
+            fn get_id() -> &'static str {
+                #fn_name
             }
         }
     };
