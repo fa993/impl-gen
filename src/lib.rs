14467b3d7422164fb899f188bfe9448ae41768ef
ameyakore <kore.ameya@gmail.com>
2022-09-05 23:24:55 +0530

    testing pt 3

--- src/lib.rs
+++ src/lib.rs
@@ -24,23 +24,23 @@ pub fn impl_gen(attr: TokenStream, item: TokenStream) -> TokenStream {
     // let struct_ast = syn::parse(item).expect("Struct failed");
     // println!("Hello pt2");
 
-    impl_gen_macro(&attr, &item);
+    for t in item.into_iter() {
+        println!("{}", t);
+    };
+
+    impl_gen_macro(&attr, unimplemented!());
 
     todo!()
 
 }
 
-fn impl_gen_macro(attrs: &TokenStream, structs: &TokenStream) -> TokenStream {
+fn impl_gen_macro(attrs: &TokenStream, name: &str) -> TokenStream {
     let str_name = attrs.to_string();
     let names = str_name.split(',').collect::<Vec<_>>();
-    // let name = &struct_ast.ident;
-    for t in structs.into_iter() {
-        println!("{}", t);
-    };
     let fn_name = format_ident!("get_{}", names[1].trim().to_case(Case::Snake));
     let trait_name = format_ident!("{}", names[0].trim());
     let gen = quote! {
-        impl #trait_name for Asd {
+        impl #trait_name for #name {
             fn get_id() -> &'static str {
                 #fn_name
             }
